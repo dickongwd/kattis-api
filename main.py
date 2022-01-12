1c0eeee56838934a7285f3618dcb9a2641a6c59c
Dick Ong <dickongwd@gmail.com>
2022-01-12 20:23:07 +0800

    Add temp testing script

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+from base64 import b64decode
+
+from kattis.user import KattisUser
+
+
+def main():
+    username = os.environ.get('KATTIS_USER')
+    password = b64decode(os.environ.get('KATTIS_PASS')
+                .encode('utf-8')).decode('utf-8')
+    
+    print('Authenticating...')
+    user = KattisUser(username, password)
+    print('Logged in successfully!')
+
+    print('Getting all solved problems...')
+    all_problems = user.solved_problems()
+    print('Done!')
+
+    total = 0
+    print('Writing to file...')
+    with open('solve.txt', 'w') as fp:
+        for data in all_problems:
+            total += data['difficulty']
+            fp.write(str(data))
+            fp.write('\n')
+    print('All done!')
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
